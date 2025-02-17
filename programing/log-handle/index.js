181d9c584c0458bbe37f36b428aa52ae9285a6a7
wan <conut22@naver.com>
2025-02-17 15:23:34 +0900

    Add: fs모듈을 사용하여 log파일 읽기, Es모듈 사용 모드 설정, log파일 추가

--- programing/log-handle/index.js
+++ programing/log-handle/index.js
@@ -1,1 +1,14 @@
-//미션
+import { readFileSync } from 'fs';
+import path from 'path';
+
+//파일 경로
+function getFilePath(fileName, fileFormat) {
+  const currentDir = new URL('.', import.meta.url).pathname;
+  return path.join(currentDir, fileName + '.' + fileFormat);
+}
+
+const FILE_NAME = getFilePath('1701410305471system', 'log');
+
+const logData = readFileSync(FILE_NAME, 'utf-8');
+
+console.log(logData);
