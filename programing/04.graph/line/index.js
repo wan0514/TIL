ed70a3380456f95c666486a3c3d7d236bcdb6be7
wan <conut22@naver.com>
2025-02-18 17:37:04 +0900

    Add: 입력값 변환 함수
    string으로 들어오는 입력값을[[x,y],[x,y]] 형태로 변환하는 parseCoordinates 함수를 생성했습니다.

--- programing/04.graph/line/index.js
+++ programing/04.graph/line/index.js
@@ -21,9 +21,25 @@ rl.on('close', () => {
   console.log('프로그램 종료');
 });
 
+//main
 function main(input) {
-  console.log(`input is: ${input}`);
+  const result = parseCoordinates(input);
+  console.log(result);
 }
 
 // 프로그램 실행
 prompt();
+
+// 변환함수 : 입력값 '(x,y)-(x,y)' -> [[x, y], [x, y]]
+function parseCoordinates(input) {
+  // 01. -을 기준으로 나눈다. ['(x,y)','(x,y)']
+  const coordinatePairs = input.split('-');
+  // 02. ()를 제거하고 ','를 기준으로 x와 y를 배열에 넣는다. [[x,y],[x,y]]
+  const coordinates = coordinatePairs.map((pair) => {
+    // 좌표 쌍에서 양옆 공백을 제거하고 괄호를 정규식으로 제거하고 ','를 기준으로 x와 y를 나눈다
+    return pair.trim().replace(/[()]/g, '').split(',').map(Number);
+  });
+
+  // 03. 반환
+  return coordinates;
+}
