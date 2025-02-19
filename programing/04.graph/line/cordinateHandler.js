213a5579544b1cca545aa7bd29f8506988bcd9e5
wan <conut22@naver.com>
2025-02-19 14:32:58 +0900

    Refactor: 다형성과 상속을 class 객체에 적용
    기존에는 상속을 사용하지 않고 개별 class로 생성한 구조에서 상속을 받고 같은 매서드를 사용하게끔 변경하였습니다.

--- programing/04.graph/line/cordinateHandler.js
+++ programing/04.graph/line/cordinateHandler.js
@@ -1,41 +1,3 @@
-// 이전 설계: 하나의 클래스로 만들었음
-// class CoordinateHandler {
-//   constructor(array = []) {
-//     this.array = array;
-//   }
-
-//   printGraph() {
-//     console.log('그래프...');
-//   }
-
-//   getDistance(index1, index2) {
-//     const coord1 = this.array[index1]; // 배열에서 첫 번째 좌표
-//     const coord2 = this.array[index2]; // 배열에서 두 번째 좌표
-//     return Math.sqrt((coord1.x - coord2.x) ** 2 + (coord1.y - coord2.y) ** 2); // 거리 계산
-//   }
-
-//   // 삼각형 넓이를 구하는 메서드
-//   calculateTriangleArea() {
-//     if (this.array.length !== 3) {
-//       throw new Error('삼각형 넓이를 구하려면 3개의 좌표가 필요합니다.');
-//     }
-
-//     // 세 점 간의 거리 계산
-//     const a = this.getDistance(0, 1);
-//     const b = this.getDistance(1, 2);
-//     const c = this.getDistance(2, 0);
-
-//     // 헤론 공식을 이용한 삼각형 넓이 계산
-//     const s = (a + b + c) / 2; // 반둘레
-//     const area = Math.sqrt(s * (s - a) * (s - b) * (s - c)); // 넓이 계산
-
-//     return Math.trunc(Number(area.toFixed(2)));
-//   }
-// }
-
-// export { CoordinateHandler };
-
-// 새롤운 설계: point,line,triangle로 클래스 구분
 class Point {
   constructor(x, y) {
     this.x = x;
@@ -43,23 +5,32 @@ class Point {
   }
 }
 
-class Line {
-  constructor(point1, point2) {
-    this.point1 = point1;
-    this.point2 = point2;
+class Shape {
+  constructor(points) {
+    this.points = points;
   }
 
-  getDistance() {
-    return Math.sqrt(
-      (this.point1.x - this.point2.x) ** 2 +
-        (this.point1.y - this.point2.y) ** 2
+  calculateArea() {
+    throw new Error(
+      'calculateArea()는 반드시 하위 클래스에서 구현해야 합니다.'
     );
   }
 }
 
-class Triangle {
+class Line extends Shape {
+  constructor(p1, p2) {
+    super([p1, p2]);
+  }
+
+  getDistance() {
+    const [p1, p2] = this.points;
+    return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
+  }
+}
+
+class Triangle extends Shape {
   constructor(p1, p2, p3) {
-    this.points = [p1, p2, p3];
+    super([p1, p2, p3]);
     this.lines = [new Line(p1, p2), new Line(p2, p3), new Line(p3, p1)];
   }
 
@@ -71,4 +42,31 @@ class Triangle {
   }
 }
 
-export { Point, Line, Triangle };
+class Polygon extends Shape {
+  constructor(points) {
+    if (points.length < 4) {
+      throw new Error('다각형은 최소 4개의 점이 필요합니다.');
+    }
+    super(points);
+  }
+
+  calculateArea() {}
+}
+
+//팩토리 객체
+class ShapeFactory {
+  static createShape(points) {
+    switch (points.length) {
+      case 2:
+        return new Line(points[0], points[1]);
+      case 3:
+        return new Triangle(points[0], points[1], points[2]);
+      case 4:
+        return new Rectangle(points[0], points[1], points[2], points[3]);
+      default:
+        return new Polygon(points);
+    }
+  }
+}
+
+export { Point, Shape, Line, Triangle, Polygon, ShapeFactory };
