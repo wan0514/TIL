c5bc56e97dc934460eef392ed11e85024629de18
wan <conut22@naver.com>
2025-02-20 15:41:10 +0900

    Add: 메모리 할당 기능, baseAddress 랜덤 선언 기능
    기존 baseAddress를 0으로 초기화시켰으나, 요구사항에 맞지 않아 heapSize안에서 랜덤하게 배정하게 했습니다.
    malloc함수로 메모리에 데이터를 할당할 수 있고, 윈도우 슬라이드 기법으로 연속된 빈 메모리를 찾게 구현했습니다.

--- programing/06.heap/index.js
+++ programing/06.heap/index.js
@@ -14,9 +14,9 @@ class Heap {
       isAllocated: false,
     }));
 
-    this.baseAddress = `0x${Math.floor(
-      Math.random() * (0xa000 - 0x0000 + 1) + 0x0000
-    ).toString(16)}`;
+    // baseAddress가 heapSize를 넘어가지 않도록 제한
+    const randomBase = Math.floor(Math.random() * (this.heapSize / 2));
+    this.baseAddress = `0x${randomBase.toString(16)}`;
 
     return this.baseAddress;
   }
@@ -40,6 +40,46 @@ class Heap {
   // 메모리 할당
   malloc(type, count) {
     // TODO: 메모리 할당 코드
+
+    if (!this._types[type]) {
+      throw new Error('Invalid type');
+    }
+
+    const typeLength = this._types[type]; // 타입 길이를 가져옴
+    let totalSize = typeLength * count;
+
+    // 작은 타입은 8바이트로 패딩을 추가
+    if (typeLength < 8) {
+      totalSize = Math.ceil(totalSize / 8) * 8; // 패딩을 추가하여 8바이트 단위로 맞춤
+    }
+
+    // 할당을 시작할 위치를 찾기
+    const baseIndex = parseInt(this.baseAddress, 16); // baseAddress를 10진수로 변환
+    let startIndex = -1; // defualt: -1 존재 안한다는 뜻
+
+    for (let i = baseIndex; i <= this.heapSize - totalSize; i++) {
+      const isAbleToAllocate = this.heap
+        .slice(i, i + totalSize)
+        .every((block) => block.isAllocated === false);
+
+      if (isAbleToAllocate) {
+        startIndex = i;
+        break; // 공간을 찾았으면 반복문을 빠져나오기
+      }
+    }
+
+    if (startIndex === -1) {
+      throw new Error('Not enough memory to allocate');
+    }
+
+    //할당
+    for (let i = startIndex; i < startIndex + totalSize; i++) {
+      this.heap[i] = { type, size: totalSize, isAllocated: true };
+    }
+
+    const relativeAddress = startIndex - baseIndex;
+
+    return this.decimalToHex(relativeAddress);
   }
 
   // 메모리 해제
@@ -56,7 +96,12 @@ class Heap {
   set types(newType) {
     // TODO: types 설정 코드
   }
-}
 
-const myHeap = new Heap();
-myHeap.init(1024);
+  decimalToHex(decimal) {
+    return `0x${decimal.toString(16).toUpperCase()}`;
+  }
+
+  getBaseAddressToHex() {
+    return `0x${this.baseAddress.toString(16).toUpperCase()}`;
+  }
+}
