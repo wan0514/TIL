481c225b3c7330bb7382024da3d57a965aeea464
wan <conut22@naver.com>
2025-02-24 21:47:52 +0900

    Add: 반시계/시계방향 읽기 , 위/아래 읽기 기능

--- programing/08./test.js
+++ programing/08./test.js
@@ -1,49 +1,49 @@
-function readCounterclockwise(rowCount, colCount, input) {
-  let result = [];
-
-  // 맨 오른쪽 열을 아래에서 위로 읽기
-  for (let i = colCount; i >= colCount - 1; i--) {
-    for (let j = rowCount; j >= rowCount - 1; j--) {
-      result.push(input[i][j]);
-    }
-  }
-
-  // 맨 왼쪽에서 두번째 열을 위에서 아래로 읽기
-  for (let i = colCount - 1; i <= colCount; i++) {
-    for (let j = rowCount - 2; j >= rowCount - 3; j--) {
-      result.push(input[i][j]);
-    }
-  }
-
-  return result.join('');
+function readCounterclockwise(colCount, rowCount, input) {
+  return (
+    readUp(colCount, rowCount, input) +
+    readDown(colCount - 1, rowCount - 2, input)
+  );
 }
 
-function readClockwise(rowCount, colCount, input) {
-  let result = [];
+function readClockwise(colCount, rowCount, input) {
+  return (
+    readDown(colCount, rowCount, input) +
+    readUp(colCount + 1, rowCount - 2, input)
+  );
+}
 
-  // 맨 오른쪽 열을 위에서부터 아래로 읽기
-  for (let i = colCount - 1; i <= colCount; i++) {
-    for (let j = rowCount; j >= rowCount - 1; j--) {
-      result.push(input[i][j]);
-    }
-  }
-
-  // 맨 왼쪽에서 두번째 열을 아래에서 위로 읽기
+function readUp(colCount, rowCount, input) {
+  let result = '';
   for (let i = colCount; i >= colCount - 1; i--) {
-    for (let j = rowCount - 2; j >= rowCount - 3; j--) {
-      result.push(input[i][j]);
+    for (let j = rowCount; j >= rowCount - 1; j--) {
+      result += input[i][j];
     }
   }
+  return result;
+}
 
-  return result.join('');
+function readDown(colCount, rowCount, input) {
+  let result = '';
+  for (let i = colCount; i <= colCount + 1; i++) {
+    for (let j = rowCount; j >= rowCount - 1; j--) {
+      result += input[i][j];
+    }
+  }
+  return result;
 }
 
 // 예시 배열 (10x10)
 const input = [
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 18, 19],
 ];
 
 // 실행 예시
-console.log(readCounterclockwise(4, 1, input));
-console.log(readClockwise(4, 1, input));
+
+console.log(readCounterclockwise(1, 4, input));
+console.log(readClockwise(0, 4, input));
+
+console.log(readUp(1, 1, input)); //  col,row  6,5,1,0
+console.log(readDown(1, 1, input)); // col,row 6,5,11,10
