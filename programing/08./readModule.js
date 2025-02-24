a88d8e12fb2cc4e7154c6d39af96fd323e4d935b
wan <conut22@naver.com>
2025-02-24 21:50:41 +0900

    Modify: 모듈 파일 이름 수정, index에서 모듈 불러오기

--- programing/08./readModule.js
+++ programing/08./readModule.js
@@ -32,18 +32,4 @@ function readDown(colCount, rowCount, input) {
   return result;
 }
 
-// 예시 배열 (10x10)
-const input = [
-  [0, 1, 2, 3, 4],
-  [5, 6, 7, 8, 9],
-  [10, 11, 12, 13, 14],
-  [15, 16, 17, 18, 19],
-];
-
-// 실행 예시
-
-console.log(readCounterclockwise(1, 4, input));
-console.log(readClockwise(0, 4, input));
-
-console.log(readUp(1, 1, input)); //  col,row  6,5,1,0
-console.log(readDown(1, 1, input)); // col,row 6,5,11,10
+export { readCounterclockwise, readClockwise, readUp, readDown };
