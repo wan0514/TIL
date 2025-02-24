bd8f9e8f6415ddce4ea5cda68282a2645c13d472
wan <conut22@naver.com>
2025-02-24 12:03:26 +0900

    Add: 클로저를 활용해 판별 목록 출력 기능

--- programing/07.funtional-programing/closer.js
+++ programing/07.funtional-programing/closer.js
@@ -14,6 +14,17 @@
 - 자연수 중에서 다른 자연수의 제곱으로 표현되는 정사각수(squared) 판단 함수를 추가한다
 */
 
+import {
+  isFactor,
+  factors,
+  isPerfect,
+  isAbundant,
+  isDeficient,
+  sum,
+  equalSet,
+  isPrime,
+} from './index.js';
+
 // 1. 정사각수 함수
 // 필요한 매개변수: number
 // 반환값 : boolean(true: 정사각수, false: 정사각수 아님)
@@ -23,5 +34,53 @@ const isSquare = (number) => {
   return Number.isInteger(squareRoot);
 };
 
+// 2. 2~100 자연수를 받아 완전수,과잉수,부족수,소수,정사각수를 판별하여 목록으로 출력
+
+// 약수 목록 구하기
+
+// min~ max 숫자 배열 생성
+const getArrayMinToMax = (min, max) => {
+  return Array.from({ length: max - min + 1 }, (_, index) => min + index);
+};
+
+// 숫자를 조건별로 판별하기 (완전수, 풍족수, 부족수, 소수, 정사각수)
+// return : 판별된 이름  (ex: ['deficient', 'prime'])
+const checkCategory = (number, factors, sum, equalSet) => {
+  const categories = [
+    { name: 'deficient', check: isDeficient(number, factors, sum) },
+    { name: 'abundant', check: isAbundant(number, factors, sum) },
+    { name: 'perfect', check: isPerfect(number, factors, sum) },
+    { name: 'prime', check: isPrime(number, factors, equalSet) },
+    { name: 'squared', check: isSquare(number) },
+  ];
+
+  //판별할 함수를 전부 체크한 후, true인 항목들만 filter. 그 항목들에서 name만 추출
+  return categories
+    .filter((category) => category.check)
+    .map((category) => category.name);
+};
+
+//출력 함수 : 클로저 사용
+const printResult = (number) => (categories) => {
+  if (categories.length > 0) {
+    console.log(`${number} : ${categories.join(', ')}`);
+  }
+};
+
 // ===== test =====
-console.log(isSquare(4));
+
+// 2부터 100까지 숫자 배열 만들기
+const numbers = getArrayMinToMax(2, 100);
+
+// 각 숫자에 대해 약수 목록 구하기
+const factorsList = numbers.map((number) => factors(number, isFactor));
+
+// 판별 결과 목록 출력
+numbers.reduce((acc, number, index) => {
+  const factorsOfNumber = factorsList[index];
+  const categories = checkCategory(number, factorsOfNumber, sum, equalSet);
+
+  printResult(number)(categories);
+
+  return acc;
+}, []);
