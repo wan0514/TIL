c05edc0654ba777d7dfc3cc17f2e1b8f03f4e816
wan <conut22@naver.com>
2025-02-24 21:56:32 +0900

    Modify: 시작,종료 검증함수 읽기 모듈을 활용하는 것으로 변경
    기존 검증 함수를 따로 만들었던 것을 읽기 모듈을 재사용하여 검증하는 것으로 변경하였습니다.

--- programing/08./index.js
+++ programing/08./index.js
@@ -6,35 +6,25 @@
 // 전체 디코딩 함수 (위 함수를 조합하여 최종 출력)
 
 import input from './input.js';
+import {
+  readCounterclockwise,
+  readClockwise,
+  readUp,
+  readDown,
+} from './readModule.js';
 
 // QR 코드 시작 & 종료 검증 함수
 function isValidStart(input) {
-  const startPattern = [
-    { row: 20, column: 20, value: '1' },
-    { row: 20, column: 19, value: '1' },
-    { row: 19, column: 20, value: '0' },
-    { row: 19, column: 19, value: '0' },
-  ];
+  const validNumber = '1100';
+  const result = readUp(20, 20, input);
 
-  return checkPattern(input, startPattern);
+  return result === validNumber;
 }
 
 function isValidEnd(input) {
-  const endPattern = [
-    { row: 19, column: 10, value: '0' },
-    { row: 19, column: 9, value: '1' },
-    { row: 20, column: 10, value: '1' },
-    { row: 20, column: 9, value: '0' },
-  ];
-
-  return checkPattern(input, endPattern);
-}
-
-// 공통된 패턴 체크 함수
-function checkPattern(input, patternRules) {
-  return patternRules.every(
-    ({ row, column, value }) => input[row][column] === value
-  );
+  const validNumber = '0110';
+  const result = readDown(19, 10, input);
+  return result === validNumber;
 }
 
 // 2차원 배열로 반환
