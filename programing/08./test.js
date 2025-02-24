47f526c084dbfc710cb3b577674b04a837ab7070
wan <conut22@naver.com>
2025-02-24 17:48:22 +0900

    Add: 시계방향으로 읽는 기능

--- programing/08./test.js
+++ programing/08./test.js
@@ -18,6 +18,26 @@ function readCounterclockwise(rowCount, colCount, input) {
   return result.join('');
 }
 
+function readClockwise(rowCount, colCount, input) {
+  let result = [];
+
+  // 맨 오른쪽 열을 위에서부터 아래로 읽기
+  for (let i = colCount - 1; i <= colCount; i++) {
+    for (let j = rowCount; j >= rowCount - 1; j--) {
+      result.push(input[i][j]);
+    }
+  }
+
+  // 맨 왼쪽에서 두번째 열을 아래에서 위로 읽기
+  for (let i = colCount; i >= colCount - 1; i--) {
+    for (let j = rowCount - 2; j >= rowCount - 3; j--) {
+      result.push(input[i][j]);
+    }
+  }
+
+  return result.join('');
+}
+
 // 예시 배열 (10x10)
 const input = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
@@ -26,3 +46,4 @@ const input = [
 
 // 실행 예시
 console.log(readCounterclockwise(4, 1, input));
+console.log(readClockwise(4, 1, input));
