c956b909758db79fc5a5d0a5cdc4d0816426b978
wan <conut22@naver.com>
2025-02-27 00:39:07 +0900

    Add: 파일 시스템 초기화 함수
    디스크 사이즈를 매개변수로 받아 파일시스템 구현에 필요한 파일을 생성합니다. 파일의 존재여부를 판단하여 존재하지 않을 경우에만 초기화를 진행하며 파일을 생성합니다.

--- programing/09.file-system/fs.js
+++ programing/09.file-system/fs.js
@@ -15,20 +15,65 @@ function getFilePath(fileName, fileFormat) {
   return path.join(CURRENT_DIR, `${fileName}.${fileFormat}`);
 }
 
+//json 파일로 변환하는 함수
+// 분리 목적: 추후 바이너리로 변환한다면 이 함수만 수정가능하게 따로 분리
+function convertToJSON(data) {
+  return JSON.stringify(data, null, 2); // 2는 들여쓰기를 2칸씩 해주는 옵션
+}
+
+// 파일 시스템 초기화 함수(디스크 크기 설정)
+
+function initFileSystem(size) {
+  //기존 파일이 존재하는지 전부 확인
+  if (!fs.existsSync(DATA_FILE_PATH)) {
+    fs.writeFileSync(DATA_FILE_PATH, convertToJSON({ clusters: {} }));
+  }
+
+  if (!fs.existsSync(DIR_FILE_PATH)) {
+    fs.writeFileSync(
+      DIR_FILE_PATH,
+      convertToJSON({
+        root: { type: 'dir', files: {}, subdifs: {} },
+      })
+    );
+  }
+
+  if (!fs.existsSync(INFO_FILE_PATH)) {
+    fs.writeFileSync(
+      INFO_FILE_PATH,
+      convertToJSON({
+        totalSize: size,
+        usedSpace: 0,
+        freeSpace: size,
+        clusterSize: 512,
+        totalClusters: 2048,
+        fat: {},
+      })
+    );
+  }
+}
+
+// 파일 읽기
+function loadFile() {}
+
+// 파일 생성
+function saveFile() {
+  // - 첫번쨰 클라스터 찾기
+  // - 디렉토리에 파일 정보 추가
+  // - 사용 공간 업데이트
+  // - 파일 저장
+}
+
+// 디렉토리 조회
+function loadDirectory() {}
+
+// 디렉토리 생성
+function saveDirectory() {}
+
+// === test ===
+
 console.log('dataFilePath', DATA_FILE_PATH);
 console.log('dirFilePath', DIR_FILE_PATH);
 console.log('infoFilePath', INFO_FILE_PATH);
 
-// 파일 읽기
-// 파일 쓰기
-// 파일 쓰기
-// 파일 시스템 초기화 함수(디스크 크기 설정)
-
-// 파일 저장
-// - 첫번쨰 클라스터 찾기
-// - 디렉토리에 파일 정보 추가
-// - 사용 공간 업데이트
-// - 파일 저장
-
-// 디렉토리 조회
-// 디렉토리 생성
+initFileSystem(500);
