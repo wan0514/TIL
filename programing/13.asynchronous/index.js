b4c537467385428496c43d0f9e0b5a9cd1daf4ab
wan <conut22@naver.com>
2025-07-17 21:36:14 +0900

    Add(asyn): 주문 형태를 n:n 형태로 하여 음료이름을 맵핑하게 변경

--- programing/13.asynchronous/index.js
+++ programing/13.asynchronous/index.js
@@ -9,6 +9,12 @@ function delay(ms, message) {
   });
 }
 
+const beberageMap = {
+  1: "아메리카노",
+  2: "라뗴",
+  3: "보드카",
+};
+
 let list = [];
 let 제조리스트 = [];
 
@@ -20,34 +26,44 @@ class MyServer extends EventEmitter {}
 
 const server = new MyServer();
 
-server.on("request", async ({ data }) => {
+server.on("request", async (주문) => {
   console.log("주문요청 완료");
 
+  const [음료번호, 개수] = 주문.split(":");
+
+  const 음료이름 = beberageMap[음료번호];
+
   // 리스트에 업데이트
-  server.emit("listUpdated", data);
+  server.emit("listUpdated", 음료이름, 개수);
 });
 
 // 업데이트 이벤트 등록
-server.on("listUpdated", (data) => {
-  list.push(data);
-  // 리스트 출력
-  console.log(list);
+server.on("listUpdated", (음료이름, 개수) => {
+  for (let i = 0; i < 개수; i++) {
+    list.push(음료이름);
+    queue.push(음료이름);
+  }
 
-  server.emit("제조중", data);
+  // // 리스트 출력
+  console.log("주문목록: ", list);
+
+  server.emit("제조중");
 });
 
-server.on("제조중", async (숫자) => {
-  queue.push(숫자);
+server.on("제조중", () => {
   processNext();
 });
 
 async function processNext() {
   if (currentProcessing >= MAX_CONCURRENT || queue.length === 0) return;
 
-  const 숫자 = queue.shift(); // 큐에서 하나 꺼냄
+  const 음료이름 = queue.shift(); // 큐에서 하나 꺼냄
+
   currentProcessing++;
 
-  await delay(2000, `${숫자} 제조완료`);
+  await delay(2000, `${음료이름} 제조완료`);
+
+  list.shift();
 
   currentProcessing--;
 
@@ -55,18 +71,18 @@ async function processNext() {
   processNext();
 }
 
-server.emit("request", { data: 1 });
-server.emit("request", { data: 2 });
-server.emit("request", { data: 3 });
+server.emit("request", "1:2");
+server.emit("request", "1:1");
+server.emit("request", "3:1");
 
 setTimeout(() => {
-  server.emit("request", { data: 4 });
+  server.emit("request", "2:3");
 }, 3000);
 
 setTimeout(() => {
-  server.emit("request", { data: 5 });
+  server.emit("request", "1:1");
 }, 3000);
 
 setTimeout(() => {
-  server.emit("request", { data: 6 });
+  server.emit("request", "2:3");
 }, 4000);
